751c45ec4eb24d2b8fb7e08b4e9dae055b4898bd
Alvaroo5 <145764969+Alvaroo5@users.noreply.github.com>
2023-10-06 13:47:26 +0200

    Create Tarea2.java

---
+++ Tarea2.java
@@ -0,0 +1,41 @@
+package TareasEntorno;
+
+import java.util.Scanner;
+
+public class Tarea2 {
+	
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		   
+		   
+		   /** Inicializo las variables */
+				float distance;
+				float velocity;
+				int timeHour;
+				int timeMinutes;
+				
+				
+				/** Obtengo los datos */
+				System.out.println("Introduce la distancia a recorrer");
+				distance = scanner.nextFloat();
+				System.out.println("Introduce la velocidad media prevista (km/h)");
+				velocity = scanner.nextFloat();
+				
+				if(velocity <= 0) {
+					System.out.println("La velocidad introducida debe ser un numero positivo, porfavor introduce un valor valido:");
+					
+					velocity = scanner.nextFloat();
+				}
+				
+				/** Calculamos el tiempo */
+				
+				timeHour= (int)(distance/velocity);
+				timeMinutes= (int)(distance % velocity);
+
+				/** Muestro los resultados */
+				System.out.println("El tiempo que se tardará en recorrer esa distancia es: "+timeHour+" horas y "+timeMinutes+" minutos");
+		
+	}
+
+}
