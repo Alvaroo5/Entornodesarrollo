366c566cff4bfb1b343cbb4d24f39c2b0096eea7
Alvaroo5 <145764969+Alvaroo5@users.noreply.github.com>
2023-10-06 14:31:09 +0200

    Create Tarea4.java

---
+++ Tarea4.java
@@ -0,0 +1,41 @@
+package TareasEntorno;
+import java.util.Scanner;
+public class Tarea4 {
+	
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		int numDia;
+		System.out.println("Introduce un número del 1 al 7 y calcularemos el dia de la semana equivalente");
+		numDia = (int)scanner.nextInt();
+		while (numDia < 1 || numDia > 7){
+		System.out.println("El número no es válido, por favor introduce un numero entre el 1 y el 7");
+		numDia = (int)scanner.nextInt();
+		
+		}
+	
+		if (numDia == 1) {
+		System.out.println("Lunes");
+		
+		}
+		if (numDia == 2) {
+		System.out.println("Martes");
+		}
+		if (numDia == 3) {
+		System.out.println("Miércoles");
+		}
+		if (numDia == 4) {
+		System.out.println("Jueves");
+		}
+		if (numDia == 5) {
+		System.out.println("Viernes");
+		}
+		if (numDia == 6) {
+		System.out.println("Sábado");}
+		
+		if (numDia == 7) {
+		System.out.println("Domingo");
+		 }
+	}
+}
